f15e0532ebcc13c09428cd2d534d59dc99dbfc58
nnapper <alvinlyao@gmail.com>
2024-03-04 12:00:47 -0800

    Changed catapult port due to errors with resetting position

--- src/robot_functions_initialization.cpp
+++ src/robot_functions_initialization.cpp
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "main.h" // External libraries
 
 pros::Controller controller(pros::E_CONTROLLER_MASTER); // Controller
 
@@ -6,5 +6,6 @@ pros::ADIDigitalOut wingLeft('A', false); // Pneumatic for the left wing
 pros::ADIDigitalOut wingRight('B', false); // Pneumatic for the right wing
 pros::ADIDigitalOut hang('C', false); // Pneumatic for the hang
 
-pros::Motor cata(21, pros::E_MOTOR_GEAR_RED, true, pros::E_MOTOR_ENCODER_DEGREES); // Motor for catapult
+pros::Motor cata(17, pros::E_MOTOR_GEAR_RED, true, pros::E_MOTOR_ENCODER_DEGREES); // Motor for catapult
 pros::Motor intake(10, pros::E_MOTOR_GEAR_RED, true, pros::E_MOTOR_ENCODER_DEGREES); // Motor for intake
+
